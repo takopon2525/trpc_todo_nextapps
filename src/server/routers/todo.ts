b22a09dfe98781bdeb373ef6385a6f68adcda815
takopon <doluche.avalanche@gmail.com>
2022-11-14 17:21:58 +0900

    まずはtodoをとってくるところから

--- src/server/routers/todo.ts
+++ src/server/routers/todo.ts
@@ -3,11 +3,11 @@ import { publicProcedure, router } from "../trpc";
 import { prisma } from "../../server/utils/prisma";
 
 export const todoRouter = router({
-    all:publicProcedure.query(async()=>{
-       return await prisma.task.findMany({
-        orderBy: {
-            createdAt: 'asc',
-          },
-       })
-    })
-})
+  all: publicProcedure.query(async () => {
+    return await prisma.task.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+  }),
+});
