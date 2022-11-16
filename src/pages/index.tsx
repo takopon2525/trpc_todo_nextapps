c92b078787173cb9866ff5f270b17929c7a0f830
takopon <doluche.avalanche@gmail.com>
2022-11-17 06:36:51 +0900

    ちょい変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,13 @@ export default function IndexPage() {
   if (!todo.data) return <div>Loading...</div>;
   return (
     <div>
+      <h3>登録されているtodo</h3>
       {todo.data.map((item, i) => (
-        <p key={i}>{item.text}</p>
+        <p key={i}>
+          {i + 1}件目:{item.text}
+        </p>
       ))}
+      <h3>新しいtodoの登録</h3>
     </div>
   );
 }
