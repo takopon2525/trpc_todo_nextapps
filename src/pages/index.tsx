b9234ec21e90ef6a11e7cf07636c9d0a96f6b695
takopon <doluche.avalanche@gmail.com>
2022-11-21 03:07:23 +0900

    必要なコンポーネントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Todos from "@/components/Todos";
 import { getAllJSDocTagsOfKind } from "typescript";
 import { trpc } from "../utils/trpc";
 
@@ -5,16 +6,8 @@ export default function IndexPage() {
   const todo = trpc.todo.all.useQuery();
   if (!todo.data) return <div>Loading...</div>;
   return (
-    <div className="mx-auto max-w-6xl flex justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h3 className="bg-slate-500">登録されているtodo</h3>
-      {todo.data.map((item, i) => (
-        <p key={i}>
-          {i + 1}件目:{item.text}
-        </p>
-      ))}
-      <h3>新しいtodoの登録</h3>
-      <button className="bg-indigo-300 py-2">aaaa</button>
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <Todos />
     </div>
   );
 }
