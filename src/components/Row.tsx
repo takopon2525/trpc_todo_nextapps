b418813ce7ef4f6a1b6fe21f8203ee72f513082b
takopon <doluche.avalanche@gmail.com>
2022-11-25 11:23:29 +0900

    rowコンポーネントを作成

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -15,7 +15,7 @@ export const Row: FC<TaskProps> = ({
   return (
     <div
       className={`flex w-full p-4 mb-2 justify-between items-center rounded ${
-        task.completed ? "bg-gray-400 " : "bg-blue-300"
+        task.completed ? "bg-gray-400 " : "bg-sky-300"
       }`}
     >
       <p
@@ -25,6 +25,20 @@ export const Row: FC<TaskProps> = ({
       >
         {task.text}
       </p>
+      <div className="flex justify-between items-center mr-2">
+        <button
+          className="h-7 w-7 flex justify-center items-center bg-red-400 hover:bg-red-500 text-white font-bold rounded"
+          onClick={() => handleDeleteTodo(task.id)}
+        >
+          X
+        </button>
+        <input
+          type="checkbox"
+          checked={task.completed}
+          onChange={() => handleCheckTodo(task.id)}
+          className="form-checkbox h-7 w-7"
+        />
+      </div>
     </div>
   );
 };
