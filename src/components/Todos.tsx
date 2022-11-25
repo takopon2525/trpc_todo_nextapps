fde9a7a6e371b7cfe0e732f4dc899589b5f09060
takopon <doluche.avalanche@gmail.com>
2022-11-25 11:30:22 +0900

    footer部分の作成

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -5,16 +5,13 @@ import { v4 as uuidv4 } from "uuid";
 import { Row } from "./Row";
 
 export const Todos: FC<{ tasks: Task[] }> = (props) => {
+  const hasTodos = props.tasks.length > 0;
+  const remainingTodos = props.tasks.filter((task) => !task.completed).length;
   const handleCheckTodo = () => {};
   const handleDeleteTodo = () => {};
   return (
-    <div>
-      <h3>this is todo lists</h3>
-      <ul>
-        {props?.tasks?.map((item, i) => (
-          <li key={i}>todo:{item.text}</li>
-        ))}
-      </ul>
+    <section className="w-10/12 lg:w-1/2 max-w-2xl flex flex-col items-center">
+      <div className="h-10" />
       {props.tasks.map((task) => (
         <Row
           key={task.id}
@@ -23,6 +20,12 @@ export const Todos: FC<{ tasks: Task[] }> = (props) => {
           handleDeleteTodo={() => handleDeleteTodo}
         />
       ))}
-    </div>
+      {!hasTodos && <p>Please add a todo!</p>}
+      {hasTodos && (
+        <p>
+          [{remainingTodos} of {props.tasks.length}] todos remaining
+        </p>
+      )}
+    </section>
   );
 };
