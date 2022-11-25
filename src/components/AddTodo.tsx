db7e077ed29e2d0f2aa7440ed7bf852d11523e75
takopon <doluche.avalanche@gmail.com>
2022-11-25 17:39:15 +0900

    add componentの追加

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,8 +1,7 @@
-import { Task } from "@prisma/client";
 import { ChangeEvent, FC, FormEvent } from "react";
 
 type AddTodoProps = {
-  task: Task;
+  task: string;
   handleSumitTodo: (e: FormEvent) => void;
   handleChange: (e: ChangeEvent) => void;
 };
@@ -12,5 +11,21 @@ export const AddTodo: FC<AddTodoProps> = ({
   handleSumitTodo,
   handleChange,
 }) => {
-  return <div>AddTodo</div>;
+  return (
+    <form className="flex justify-between w-full" onSubmit={handleSumitTodo}>
+      <input
+        type="text"
+        name="task"
+        value={task}
+        className="flex-1 rounded shadow p-2 text-grey-dark mr-2"
+        onChange={handleChange}
+      />
+      <button
+        className="mr-2 h-9 w-9 flex justify-center items-center bg-sky-400 hover:bg-sky-500 text-white font-bold rounded"
+        type="submit"
+      >
+        ＋
+      </button>
+    </form>
+  );
 };
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,15 +2,33 @@ import { Task } from "@prisma/client";
 import { ChangeEvent, FC, FormEvent, useState } from "react";
 import { isTemplateExpression } from "typescript";
 import { v4 as uuidv4 } from "uuid";
+import { AddTodo } from "./AddTodo";
 import { Row } from "./Row";
 
 export const Todos: FC<{ tasks: Task[] }> = (props) => {
+  const [task, setTask] = useState("");
   const hasTodos = props.tasks.length > 0;
   const remainingTodos = props.tasks.filter((task) => !task.completed).length;
+
+  const handleChange = (e: ChangeEvent) => {
+    const { value } = e.target as HTMLInputElement;
+    setTask(value);
+  };
+
+  const handleSubmitTodo = (e: FormEvent) => {
+    e.preventDefault();
+    // TODO:ここにデータベースに入れるメソッドを追加
+  };
+
   const handleCheckTodo = () => {};
   const handleDeleteTodo = () => {};
   return (
     <section className="w-10/12 lg:w-1/2 max-w-2xl flex flex-col items-center">
+      <AddTodo
+        task={task}
+        handleChange={handleChange}
+        handleSumitTodo={handleSubmitTodo}
+      />
       <div className="h-10" />
       {props.tasks.map((task) => (
         <Row
