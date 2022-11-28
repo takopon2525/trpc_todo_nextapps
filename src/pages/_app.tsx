9db3c9f90aa3460c888368dac1af16cf8c9e2259
takopon <doluche.avalanche@gmail.com>
2022-11-29 07:37:49 +0900

    a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,10 @@ import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
-import HeaderImg from "../assets/headerImg.svg";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const title = "TODOアプリを作成して学ぶ";
+  const imgurl = "https://trpc-todo-nextapps.vercel.app/headerImg";
   const description = "tRPCとsupabseを使ってTODOアプリを作ってみましょう！";
   return (
     <div>
@@ -15,7 +15,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <title>Todos</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content={title} />
-        <meta property="og:image" content={HeaderImg} />
+        <meta property="og:image" content={imgurl} />
         <meta property="og:description" content={description} />
       </Head>
       <main>
