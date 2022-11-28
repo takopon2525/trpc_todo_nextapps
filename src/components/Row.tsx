ff2997af50cd2c6374bcc3e441e7a75dbeb52403
takopon <doluche.avalanche@gmail.com>
2022-11-29 05:19:54 +0900

    修正

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,41 +1,69 @@
-import { FC } from "react";
+import { FC, use, useState } from "react";
 import { Task } from "@prisma/client";
+import { trpc } from "@/utils/trpc";
 
 type TaskProps = {
   task: Task;
-  handleCheckTodo: (id: string) => void;
-  handleDeleteTodo: (id: string) => void;
 };
 
-export const Row: FC<TaskProps> = ({
-  task,
-  handleCheckTodo,
-  handleDeleteTodo,
-}) => {
+export const Row: FC<TaskProps> = ({ task }) => {
+  const utils = trpc.useContext();
+  const [editing, setEditing] = useState(false);
+  const [text, setText] = useState(task.text);
+  const [] = useState(task.completed)
+  const editTask = trpc.todo.edit.useMutation({
+    async onMutate({ id, data }) {
+      await utils.todo.all.cancel();
+      const allTasks = utils.todo.all.getData();
+      if (!allTasks) return;
+      utils.todo.all.setData(
+        undefined,
+        allTasks.map((t) => (t.id === id ? { ...t, ...data } : t))
+      );
+    },
+  });
+
   return (
     <div
       className={`flex w-full p-4 mb-2 justify-between items-center rounded ${
         task.completed ? "bg-gray-400 " : "bg-sky-300"
       }`}
     >
-      <p
-        className={`ml-2 text-xl font-sans font-medium ${
+      {/* 編集 */}
+      <input
+        className={`ml-2 text-xl font-sans font-medium w-full rounded-l ${
           task.completed ? "text-white line-through" : "text-gray-700"
         }`}
-      >
-        {task.text}
-      </p>
+        value={text}
+        onChange={(e) => {
+          const newText = e.currentTarget.value;
+          setText(newText);
+        }}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            editTask.mutate({
+              id: task.id,
+              data: { text },
+            });
+            setEditing(false);
+          }
+        }}
+      />
       <div className="flex justify-between items-center mr-2">
         <button
           className="mr-2 h-7 w-7 flex justify-center items-center bg-red-400 hover:bg-red-500 text-white font-bold rounded"
-          onClick={() => handleDeleteTodo(task.id)}
+          onClick={() => {}}
         >
           X
         </button>
+        {/* todoの完了 */}
         <input
           type="checkbox"
           checked={task.completed}
-          onChange={() => handleCheckTodo(task.id)}
+          onChange={(e) => {
+            const checked = e.currentTarget.checked
+            
+          }}
           className="form-checkbox h-7 w-7"
         />
       </div>
