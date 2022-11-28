00166ef6330ce6516d48ddc64799767308b7c22e
takopon <doluche.avalanche@gmail.com>
2022-11-29 07:49:35 +0900

    head部分は不要

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,11 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import { Toaster } from "react-hot-toast";
-import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const title = "TODOアプリを作成して学ぶ";
-  const imgurl = "https://trpc-todo-nextapps.vercel.app/headerImg.png";
-  const description = "tRPCとsupabseを使ってTODOアプリを作ってみましょう！";
   return (
     <div>
       <Toaster />
-      <Head>
-        <title>Todos</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content={title} />
-        <meta property="og:image" content={imgurl} />
-        <meta property="og:description" content={description} />
-      </Head>
       <main>
         <Component {...pageProps} />
       </main>
